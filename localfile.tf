3b242706adb41f9a05da96929d55c4ef4ad65e7b
cvalencia86 <cristian.valencia@e360.com>
2025-09-17 11:31:23 -0400

    added comment to local.tf file

--- localfile.tf
+++ localfile.tf
@@ -1,3 +1,5 @@
+# This is an example of using the local file provider to create a file on the local filesystem
+# The state file will be stored locally, so this is not suitable for collaborative environments
 resource "local_file" "example" {
   content  = "This is an example of a local file created by Terraform."
   filename = "C:/Temp/example.txt"
